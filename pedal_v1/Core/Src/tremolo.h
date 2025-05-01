6fcdf65b3aee703b16cb8afd2465d87159c244b9
msongweiss <msongweiss@olin.edu>
2025-05-01 16:13:01 -0400

    Update tremolo.c and .h

--- pedal_v1/Core/Src/tremolo.h
+++ pedal_v1/Core/Src/tremolo.h
@@ -7,10 +7,17 @@
 #include <math.h>
 
 typedef struct {
-    float rate;
-    float depth;
+	float T;
+    float rate;		// LFO Freq (Hz)
+    float depth;	// Tremolo depth, 0.0 to 1.0
+    float phase;	// Phase of modulation wave
     
-    float out;
-} Tremolo
+    float out;		// Output in -1f to 1f form
+} Tremolo;
+
+void Tremolo_Init(Tremolo *tr, float samplingFreqHz, float rate, float depth);
+//void Tremolo_Set_Rate();
+//void Tremolo_Set_Depth();
+float Tremolo_Update(Tremolo *tr, float inp);
 
 #endif
